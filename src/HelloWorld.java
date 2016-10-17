0626b12ffc94a874560410b45d352dfed5e40bc4
Rohan Paranjpe <rap2363@gmail.com>
2016-10-16 18:01:19 -0700

    Tabs --> Spaces

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,6 @@
 
-public class HelloWorld 
-{
-	public static void main (String[] args) {
-		System.out.println("Hello World!");
-	}
+public class HelloWorld {
+    public static void main (String[] args) {
+        System.out.println("Hello World!");
+    }
 }
