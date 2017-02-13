1e0024a6107db41f640fd83b5fe8e16de89673c2
Rohan Paranjpe <rparanjpe@uber.com>
2017-02-12 16:31:02 -0800

    Launch a screen

--- src/nes/Console.java
+++ src/nes/Console.java
@@ -1,13 +1,7 @@
 package nes;
 
-import memory.ConsoleMemory;
-
-import java.io.IOException;
-import java.util.Timer;
 import java.nio.file.Paths;
-import java.util.TimerTask;
-import javax.imageio.ImageIO;
-import java.io.File;
+import memory.ConsoleMemory;
 
 /**
  * Encompasses multiple emulated components of the entire NES (the CPU, PPU, APU, and Memory Mapper for the Cartridge)
@@ -24,34 +18,5 @@ public class Console {
         cpu = new CPU(consoleMemory);
         ppu = new PPU(consoleMemory);
         ppu.setMirroringMode(cartridge.getMirroringMode());
-    }
-
-    public static void main(String[] args) {
-        Console console = new Console("/Users/rparanjpe/WhiteRaven/DonkeyKong.nes");
-        System.out.println(console.cpu.state());
-
-        Timer timer = new Timer();
-        int FRAME_TIME = 17;
-        timer.scheduleAtFixedRate(new TimerTask() {
-            @Override
-            public void run() {
-                for (int i = 0; i < (1789773.0 / 60.0); i++) {
-                    console.cpu.executeCycle();
-                    console.ppu.executeCycles(3);
-                    if (console.ppu.triggerVerticalBlank) {
-                        console.cpu.triggerInterrupt(Interrupt.NMI);
-                        console.ppu.triggerVerticalBlank = false;
-                    }
-                    if (console.ppu.imageReady) {
-                        try {
-                            ImageIO.write(console.ppu.getImage(), "png", new File("background.png"));
-                        } catch (IOException e) {
-                            System.err.println("Background image not saved!");
-                            e.printStackTrace();
-                        }
-                    }
-                }
-            }
-        }, 0, FRAME_TIME);
     }
 }
